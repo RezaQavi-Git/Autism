ec54ba4f65d40e82f0ceffcc9be650f57135d8aa
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-25 17:02:50 +0430

    create Autism component

---
+++ src/components/autism/Autism.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Autism extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return(
+            <h1>Autism</h1>
+        );
+    }
+
+}
