4f5742e852e17e18336da8c872280c92d294056e
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-26 22:50:50 +0430

    change total App component  design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import './components/static/Fonts/vazir-fonts/fonts.css'
 
 import Autism from './components/fa/autism/Autism';
+import About from './components/common/about/About';
 
 class App extends React.Component {
   
@@ -15,6 +16,7 @@ class App extends React.Component {
       <Router>
         <Switch>
           <Route exact="true" path="/" component={Autism}></Route>
+          <Route exact="true" path="/about" component={About}></Route>
         </Switch>
       </Router>
     );
