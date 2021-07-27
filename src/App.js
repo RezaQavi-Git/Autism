4b1843b94b158f8d1e758b0084601e46f8119ad9
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-27 11:50:09 +0430

    Add Questionnaire component Route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './components/static/Fonts/vazir-fonts/fonts.css'
 
 import Autism from './components/fa/autism/Autism';
 import About from './components/common/about/About';
+import Questionnaire from './components/common/autism/questionnaire/Questionnaire';
 
 class App extends React.Component {
   
@@ -17,6 +18,7 @@ class App extends React.Component {
         <Switch>
           <Route exact="true" path="/" component={Autism}></Route>
           <Route exact="true" path="/about" component={About}></Route>
+          <Route exact="true" path="/questionnaire" component={Questionnaire}></Route>
         </Switch>
       </Router>
     );
