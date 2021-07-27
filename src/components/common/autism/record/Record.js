476b6519bfe5039bda41b39203ce4fe1f9c6c807
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-27 15:20:52 +0430

    redsign Record component

--- src/components/common/autism/record/Record.js
+++ src/components/common/autism/record/Record.js
@@ -3,16 +3,16 @@ import React from "react";
 import "./record.css";
 import $ from "jquery";
 
+import { ReactMic } from "react-mic";
 import Microphone from "../../../static/images/microphone.png";
 import Language from "../../header/Language";
 import Header from "../../header/Header";
 import Footer from "../../footer/Footer";
 
-import MicRecorder from "mic-recorder-to-mp3";
+// import MicRecorder from "mic-recorder-to-mp3";
 import { Link } from "react-router-dom";
 
-
-const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+// const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 class Record extends React.Component {
   constructor(props) {
@@ -34,7 +34,7 @@ class Record extends React.Component {
           <p className="seprator"></p>
           <RecordBody />
           <p className="seprator"></p>
-          <Link to="/questionnaire"> <button id="record-accepted" className="voice-accept">ثبت و ادامه</button> </Link>
+          <Link to="/questionnaire"><button id="record-accepted" className="voice-accept">ثبت و ادامه</button></Link>
           <p className="seprator"></p>
           <Footer lang="fa" />
         </div>
@@ -115,32 +115,20 @@ class RecordBox extends React.Component {
   }
 
   start = () => {
-    this.timer();
-    if (this.state.isBlocked) {
-      console.log("Permission Denied");
-    } else {
-      Mp3Recorder.start()
-        .then(() => {
-          this.setState({ status: false, isRecording: true, recorded: false });
-        })
-        .catch((e) => console.error(e));
-    }
+    this.setState({
+      status: false,
+      recorded: false,
+      record: true,
+    });
   };
 
   stop = () => {
-    clearInterval(this.Interval);
-    Mp3Recorder.stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        const blobURL = URL.createObjectURL(blob);
-        this.setState({
-          blobURL,
-          isRecording: false,
-          status: true,
-          recorded: true,
-        });
-      })
-      .catch((e) => console.log(e));
+    this.setState({
+      isRecording: false,
+      status: true,
+      recorded: true,
+      record: false,
+    });
   };
 
   showAcceptButtun() {
@@ -156,56 +144,114 @@ class RecordBox extends React.Component {
       status: true,
       recorded: false,
       isRecording: false,
-      blobURL: "",
+      blobURL: null,
       isBlocked: false,
+      record: false,
     });
   }
+
+  onStop(recordedBlob) {
+    console.log("recordedBlob is: ", recordedBlob);
+
+    console.log("recordedBlob is: ", typeof recordedBlob.blobURL);
+    this.setState({
+      blobURL: recordedBlob.blobURL,
+    });
+  }
+
   render() {
     return (
-      <div className="record">
-        <div className="recorder">
-          <div className="control-button">
-            <div className="record-voice">
-              <img
-                src={Microphone}
-                alt="mirophone"
-                onClick={this.state.status ? this.start : this.stop}
-                className="record-button"
-              ></img>
-              <p
-                className={
-                  this.state.isRecording | this.state.recorded
-                    ? "timer"
-                    : "hidden"
-                }
-              >
-                <span id="seconds">00</span>:<span id="tens">00</span>
-              </p>
-            </div>
-            <div
-              id="submit"
-              className={this.state.recorded ? "submit-voice" : "hidden"}
-            >
-              <p className="notife">آیا صدای ضبط شده مورد تایید شماست؟</p>
-              <div className="submit-voice-buttons">
-                <button
-                  className="submit button"
-                  onClick={this.showAcceptButtun.bind(this)}
-                >
-                  بله
-                </button>
-                <button
-                  className="cancel button"
-                  onClick={this.cancelVoice.bind(this)}
-                >
-                  انصراف
-                </button>
+        <div className="record">
+          <div className="recorder">
+              <div className="record-voice">
+                <ReactMic
+                  record={this.state.record}
+                  className="record-wave"
+                  onStop={this.onStop.bind(this)}
+                  strokeColor="#000000"
+                  mimeType="audio/wav"
+                  bitRate={256000}
+                  sampleRate={44100}
+                />
+                <img
+                  src={Microphone}
+                  alt="mirophone"
+                  onClick={this.state.status ? this.start : this.stop}
+                  className="record-button"
+                />
+                {/* <audio
+                  src={this.state.blobURL}
+                  controls="controls"
+                  className="record-player"
+                /> */}
               </div>
-            </div>
+              <div
+                id="submit"
+                className={this.state.recorded ? "submit-voice" : "hidden"}
+              >
+                <p className="notife">آیا صدای ضبط شده مورد تایید شماست؟</p>
+                <div className="submit-voice-buttons">
+                  <button
+                    className="submit button"
+                    onClick={this.showAcceptButtun.bind(this)}
+                  >
+                    بله
+                  </button>
+                  <button
+                    className="cancel button"
+                    onClick={this.cancelVoice.bind(this)}
+                  >
+                    انصراف
+                  </button>
+                </div>
+              </div>
+            <br />
           </div>
-          <br />
         </div>
-      </div>
+      // <div className="record">
+      //   <div className="recorder">
+      //     <div className="control-button">
+      //       <div className="record-voice">
+      //         <img
+      //           src={Microphone}
+      //           alt="mirophone"
+      //           onClick={this.state.status ? this.start : this.stop}
+      //           className="record-button"
+      //         ></img>
+      //         <p
+      //           className={
+      //             this.state.isRecording | this.state.recorded
+      //               ? "timer"
+      //               : "hidden"
+      //           }
+      //         >
+      //           <span id="seconds">00</span>:<span id="tens">00</span>
+      //         </p>
+      //       </div>
+      //       <div
+      //         id="submit"
+      //         className={this.state.recorded ? "submit-voice" : "hidden"}
+      //       >
+      //         <p className="notife">آیا صدای ضبط شده مورد تایید شماست؟</p>
+      //         <div className="submit-voice-buttons">
+      //           <button
+      //             className="submit button"
+      //             onClick={this.showAcceptButtun.bind(this)}
+      //           >
+      //             بله
+      //           </button>
+      //           <button
+      //             className="cancel button"
+      //             onClick={this.cancelVoice.bind(this)}
+      //           >
+      //             انصراف
+      //           </button>
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <br />
+      //   </div>
+      // </div>
     );
   }
 }
