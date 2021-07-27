a99a3556477eec0dee9942c13bc7219ebd620173
Reza Qavi <rezaqavi1378@gmail.com>
2021-07-27 12:43:58 +0430

    Add Record Route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './components/static/Fonts/vazir-fonts/fonts.css'
 import Autism from './components/fa/autism/Autism';
 import About from './components/common/about/About';
 import Questionnaire from './components/common/autism/questionnaire/Questionnaire';
+import Record from './components/common/autism/record/Record';
 
 class App extends React.Component {
   
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Route exact="true" path="/" component={Autism}></Route>
           <Route exact="true" path="/about" component={About}></Route>
           <Route exact="true" path="/questionnaire" component={Questionnaire}></Route>
+          <Route exact="true" path="/record" component={Record}></Route>
         </Switch>
       </Router>
     );
